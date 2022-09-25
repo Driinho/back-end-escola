e0daa769b3fb5ccf4b56d8bb36baf140956040f2
Driinho <pedro.nascimento.henrique2003@gmail.com>
2022-09-25 16:53:29 -0300

    Implementado metodo post

--- src/main/java/br/com/ifpr/escola/controllers/ProfessorController.java
+++ src/main/java/br/com/ifpr/escola/controllers/ProfessorController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,32 +15,37 @@ import br.com.ifpr.escola.models.Professor;
 import br.com.ifpr.escola.repository.ProfessorRepository;
 
 @RestController
-@RequestMapping(value = "/professor")
+@RequestMapping(value = "/postgres")
 public class ProfessorController {
 
     @Autowired
     private ProfessorRepository professorRepository;
 
-    @GetMapping
+    @GetMapping("/professor")
     public List<Professor> buscarProfessor() {
         return professorRepository.findAll();
     }
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/professor/{id}")
     public Optional<Professor> buscaProfessorPorId(@PathVariable(name = "id", required = true) long id) {
         return professorRepository.findById(id);
     }
 
-    @GetMapping(path = "/autenticar/{nomeDeUsuario}")
+    @GetMapping(path = "/professor/autenticar/{nomeDeUsuario}")
     public Optional<Professor> buscaPorNomeDeUsuario(
             @PathVariable(name = "nomeDeUsuario", required = true) String nomeDeUsuario) {
         return professorRepository.findByNomeDeUsuario(nomeDeUsuario);
     }
 
-    @GetMapping(path = "/autenticar/{nomeDeUsuario}/{senha}")
+    @GetMapping(path = "/professor/autenticar/{nomeDeUsuario}/{senha}")
     public Optional<Professor> autenticar(@PathVariable(name = "nomeDeUsuario", required = true) String nomeDeUsuario,
             @PathVariable(name = "senha", required = true) String senha) {
 
         return professorRepository.findByNomeDeUsuarioAndSenha(nomeDeUsuario, senha);
+    }
+
+    @PostMapping(path = "/professor/save")
+    public void salvaProfessor(@RequestBody Professor professor) {
+        professorRepository.save(professor);
     }
 }
